5dd681347b36908c5dc6880fcebe79319252136d
ivancotacte <cotactearmenion@gmail.com>
2024-08-24 17:08:29 +0800

    Added Code

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
+import authRoutes from './src/routes/authRoutes.js'; 
+import session from 'express-session';
 dotenv.config();
 
 const app = express();
@@ -9,7 +11,19 @@ const PORT = process.env.PORT || 3000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
+}));
 
+app.use('/', authRoutes);
+
+app.get('/', (req, res) => {
+    res.status(200).send({ code: 200, message: 'Welcome to the Auth API' });
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on PORT ${PORT}`);
